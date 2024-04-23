de726c788c818f53732786440817310d0d8d1dbe
Arthur B <arthur.holz.91@gmail.com>
2024-04-23 11:56:44 +0200

    add redirect to /graphql/

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
-from final.schema.schema import schema
+from django.views.generic import RedirectView
+
+from exercise1.schema.schema import schema
 from django.urls import path
 from strawberry.django.views import GraphQLView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='graphql/', permanent=False)),
     path('graphql/', GraphQLView.as_view(
         schema=schema,
         graphql_ide=settings.STRAWBERRY_GRAPHQL_IDE,
